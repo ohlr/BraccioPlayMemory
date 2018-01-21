c9ce6730ee3e945b52b37dbb6a308946ed0ab28d
Fee <felizitas.kunz@online.de>
2018-01-21 14:09:34 +0100

    Move images and labels to darknet folder

--- darknet/process.py
+++ darknet/process.py
@@ -16,7 +16,7 @@ file_test = open('test.txt', 'w')
 # Populate train.txt and test.txt
 counter = 1  
 index_test = round(100 / percentage_test)  
-for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.JPEG")):  
+for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg	")):  
     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
 
     if counter == index_test:
